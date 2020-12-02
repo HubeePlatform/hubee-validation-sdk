4b56c4217cbf370fd53446827fd61bbc09f6570f
João Antonio Bulgareli <jabulgareli@gmail.com>
2020-12-02 17:42:56 -0300

    Add self-validation on ValidatableSchema (#6)
    An error could be generated when a programmer creates an instance of object without calling
    ValidateSchema() method.

--- src/Hubee.Validation.Sdk/Core/Models/ValidatableSchema.cs
+++ src/Hubee.Validation.Sdk/Core/Models/ValidatableSchema.cs
@@ -1,10 +1,26 @@
-﻿using Hubee.Validation.Sdk.Core.Interfaces;
+﻿using Hubee.Validation.Sdk.Core.Extensions;
+using Hubee.Validation.Sdk.Core.Interfaces;
 
 namespace Hubee.Validation.Sdk.Core.Models
 {
-    public abstract class ValidatableSchema: IValidatableSchema
+    public abstract class ValidatableSchema : IValidatableSchema
     {
-        public ValidationResult ValidationResult { get; set; }
+        private ValidationResult validationResult;
+
+        public ValidationResult ValidationResult
+        {
+            get
+            {
+                if (validationResult is null)
+                    this.ValidadeSchema();
+
+                return validationResult;
+            }
+            set
+            {
+                validationResult = value;
+            }
+        }
 
         public abstract object GetSchemaRules();
     }
